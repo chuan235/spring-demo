15a370a92cfe93b35de9c7cac11bb1a6fbfd5c52
chuan235 <1595030095@qq.com>
2021-01-06 11:56:06 +0800

    注册MeterRegistry

--- springboot-actuator/src/main/java/top/gmfcj/ApplicationStart.java
+++ springboot-actuator/src/main/java/top/gmfcj/ApplicationStart.java
@@ -1,8 +1,12 @@
 package top.gmfcj;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
@@ -11,5 +15,10 @@ public class ApplicationStart {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(@Value("${spring.application.name}") String applicationName) {
+        return registry -> registry.config().commonTags("application", applicationName);
     }
 }
